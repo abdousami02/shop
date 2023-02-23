c2b680f377d0a397d5b96cdaef5d566e615b5f66
abdousami02 <medsami246@gmail.com>
2023-02-23 20:03:49 +0100

    add meta tag

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+    <link rel="icon" href="">
+
+    <!-- meta description -->
+    <meta name="copyright" content="BadniShop">
+    <meta name="description" content="Selling wholesale products to shop owners and supermarkets at reasonable prices and free delivery. The order is on website and payment is made upon delivery" >
+    <meta name="description" content="Vente en gros de produits aux commerçants et grandes surfaces à des prix raisonnables et livraison gratuite.La commande se fait sur web et le paiement s'effectue à la livraison" >
+    <meta name="description" content="بيع منتجات بالجملة لأصحاب المحلات و سوبرات بأثمان معقولة و التوصيل مجاني، والطلب يكون في الموقع و الدفع عند التوصيل" >
+
+    <meta name="keywords" content="badnishop badni shop gros seller achat vente ecommerce product produit location Algeria Chlef ">
+    <meta name="keywords" content="Alimantation boissons nettoyage couches bébé yaourts fromages sucreries ">
+    <meta name="keywords" content="شراء بيع بالجملة شلف الجزائر مواد غذائية موقع خدمات نقل منتجات توصيل">
+    <meta name="keywords" content="مواد غذائية مشروبات تنظيف حفاضات معلبات ياغورت اجبان حلويات">
+
+    <!-- facebook meta -->
+    <meta name="og:title" content="BadniShop" />
+    <meta name="og:type" content="Ecommerce webSite" />
+    <meta name="og:image" content='logo.png' />
+    <meta name="og:description" content="Vente en gros de produits aux commerçants" />
+    <meta name="og:description" content="بيع المنتجات بالجملة لأصحاب المحلات و السوبيرات">
+
     <link rel="stylesheet" href="/css/all.css">
     <link rel="stylesheet" href="/css/app.css">
 </head>
